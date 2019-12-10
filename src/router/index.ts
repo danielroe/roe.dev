d525417a0361ab4170c4dd54e185a94212d5820c
Daniel Roe <daniel@concision.co.uk>
2019-12-10 21:50:33 +0000

    chore: upgrade deps

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue, { VueConstructor } from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes: RouteConfig[] = [
   {
     path: '/',
     name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: (Home as unknown) as VueConstructor,
   },
 ]
 
