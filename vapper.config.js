5c9935251e07abe3ecd9f99e850d443ece7a9092
Daniel Roe <daniel@concision.co.uk>
2019-12-12 06:48:45 +0000

    refactor: rename index template

--- vapper.config.js
+++ vapper.config.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 module.exports = {
   port: process.env.PORT || '4000',
-  template: fs.readFileSync('index.html', 'utf-8'),
+  template: fs.readFileSync('index.template.html', 'utf-8'),
   plugins: [
     [
       '@vapper/plugin-prerender',
