1813a4271983e2b10fcfde8cc936debdd1ca5090
Daniel Roe <daniel@concision.co.uk>
2019-12-16 14:51:44 +0000

    fix: add a11y labels

--- src/components/layout/TheSiteHeader.vue
+++ src/components/layout/TheSiteHeader.vue
@@ -9,7 +9,7 @@
       <!-- <li><router-link to="/about">About</router-link></li> -->
       <li><router-link to="/blog">Blog</router-link></li>
     </ul>
-    <button @click="toggleTheme">
+    <button aria-label="Change site theme" @click="toggleTheme">
       <span />
     </button>
   </nav>
