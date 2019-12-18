49ab499846139bb0d5830126f254d99828c7e385
Daniel Roe <daniel@concision.co.uk>
2019-12-18 17:39:46 +0000

    refactor: extract list styling to functional component

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside :class="$style.list">
+  <ItemList>
     <router-link
       v-for="{ title, slug, date } in entries"
       :key="slug"
@@ -18,14 +18,18 @@
         </header>
       </article>
     </router-link>
-  </aside>
+  </ItemList>
 </template>
 
 <script lang="ts">
 import { createComponent } from '@vue/composition-api'
+
+import ItemList from '@/components/ItemList.vue'
+
 import { useBlogEntries } from '@/utils/blogs'
 
 export default createComponent({
+  components: { ItemList },
   props: {
     limit: {
       type: Number as () => number,
@@ -45,36 +49,3 @@ export default createComponent({
   },
 })
 </script>
-
-<style lang="postcss" module>
-.list {
-  @apply flex flex-row flex-wrap -mx-2;
-  > * {
-    @apply p-4 m-2;
-    @apply text-xl flex flex-col justify-end;
-    @apply shadow;
-    background-color: var(--accent, theme('colors.gray.900'));
-    min-height: 10rem;
-    flex: 40%;
-    @media (width < 767px) {
-      flex: 100%;
-      min-height: 3rem;
-    }
-  }
-  a {
-    transition: 0.3s transform;
-    border: 1px solid transparent;
-    transition: 0.3s border-color;
-
-    &:hover,
-    &:active,
-    &:focus {
-      @apply outline-none;
-      border-color: var(--text, theme('colors.muted'));
-    }
-  }
-  a::after {
-    @apply text-transparent;
-  }
-}
-</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,42 @@
+<template functional>
+  <aside :class="[$style.list, data.staticClass, data.class]">
+    <slot />
+  </aside>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({})
+</script>
+
+<style lang="postcss" module>
+.list {
+  @apply flex flex-row flex-wrap -mx-2;
+  > * {
+    @apply p-4 m-2 relative;
+    @apply text-xl flex flex-col justify-end;
+    @apply shadow;
+    background-color: var(--accent, theme('colors.gray.900'));
+    min-height: 10rem;
+    flex: 40%;
+    @media (width < 767px) {
+      flex: 100%;
+      min-height: 3rem;
+    }
+    transition: 0.3s transform;
+    border: 1px solid transparent;
+    transition: 0.3s border-color;
+
+    &:hover,
+    &:active,
+    &:focus {
+      @apply outline-none;
+      border-color: var(--text, theme('colors.muted'));
+    }
+    &::after {
+      @apply text-transparent;
+    }
+  }
+}
+</style>
