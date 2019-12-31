2431200b91e934f2795005299bed48801bfebaca
Daniel Roe <daniel@danielcroe.com>
2019-12-31 14:28:58 +0000

    perf: remove inessential packages (#18)
    * perf: remove inessential packages
    
    * fix: require postcss in prod
    
    * refactor: simplify theme
    
    * perf: improve theme performance
    
    * fix: allow console statements

--- postcss.config.js
+++ postcss.config.js
@@ -5,27 +5,6 @@ module.exports = {
     require('postcss-preset-env')({ stage: 0 }),
     require('postcss-nested')({}),
     require('tailwindcss')(),
-    // IN_PRODUCTION &&
-    //   require('@fullhuman/postcss-purgecss')({
-    //     content: [`./public/**/*.html`, `./src/**/*.vue`],
-    //     defaultExtractor(content) {
-    //       const contentWithoutStyleBlocks = content.replace(
-    //         /<style[^]+?<\/style>/gi,
-    //         '',
-    //       )
-    //       return (
-    //         contentWithoutStyleBlocks.match(
-    //           /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g,
-    //         ) || []
-    //       )
-    //     },
-    //     whitelist: [],
-    //     whitelistPatterns: [
-    //       /-(leave|enter|appear)(|-(to|from|active))$/,
-    //       /^(?!(|.*?:)cursor-move).+-move$/,
-    //       /^router-link(|-exact)-active$/,
-    //     ],
-    //   }),
     require('autoprefixer')(),
   ],
 }
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,53 +1,24 @@
-import { useLocalStorage } from 'vue-use-web'
-import { computed, Ref, ref, onMounted, watch } from '@vue/composition-api'
-import resolveConfig from 'tailwindcss/resolveConfig'
-// @ts-ignore
-import tailwindConfig from '../../tailwind.config.js'
+import { Ref, onMounted, watch } from '@vue/composition-api'
+import useLocalStorage from './local-storage'
 
-const fullConfig = resolveConfig(tailwindConfig)
-
-type Theme = 'dark' | 'light'
+type Theme = 'dark' | 'light' | undefined
 
 let theme: Ref<Theme>
 
 export function useTheme() {
-  if (!theme) {
-    const { value } = useLocalStorage('theme', ('dark' as unknown) as undefined)
-    theme = value
-  }
+  if (!theme) theme = useLocalStorage<Theme>('theme', 'dark', true)
 
   onMounted(() => {
-    watch(() => {
-      localStorage.setItem('theme', theme.value)
-    })
+    watch(() => localStorage.setItem('theme', theme.value || 'dark'))
   })
 
   function toggleTheme() {
-    if (theme.value === 'dark') theme.value = 'light'
+    if (!theme.value || theme.value === 'dark') theme.value = 'light'
     else theme.value = 'dark'
   }
-
-  const themeStyle = computed(() => {
-    if (theme.value === 'light') {
-      return {
-        '--background': fullConfig.theme.colors.gray['200'],
-        '--accent': fullConfig.theme.colors.gray['100'],
-        '--text-base': fullConfig.theme.colors.gray['800'],
-        '--text-muted': fullConfig.theme.colors.gray['700'],
-      }
-    } else {
-      return {
-        '--background': fullConfig.theme.colors.gray['800'],
-        '--accent': fullConfig.theme.colors.gray['900'],
-        '--text-base': fullConfig.theme.colors.white,
-        '--text-muted': fullConfig.theme.colors.gray['300'],
-      }
-    }
-  })
 
   return {
     theme,
     toggleTheme,
-    themeStyle,
   }
 }
