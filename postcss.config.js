8c31f38d020ba082e51158a2c55388c41078db79
Daniel Roe <daniel@concision.co.uk>
2019-12-31 13:20:15 +0000

    fix: require postcss in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
-const IN_PRODUCTION = process.env.NODE_ENV === 'production'
+// const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  plugins: IN_PRODUCTION
-    ? []
-    : [
-        require('postcss-preset-env')({ stage: 0 }),
-        require('postcss-nested')({}),
-        require('tailwindcss')(),
-        require('autoprefixer')(),
-      ],
+  plugins: [
+    require('postcss-preset-env')({ stage: 0 }),
+    require('postcss-nested')({}),
+    require('tailwindcss')(),
+    require('autoprefixer')(),
+  ],
 }
