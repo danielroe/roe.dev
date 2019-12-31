e550b0bee51fdd38a04fc17bff0ef15594f17896
Daniel Roe <daniel@concision.co.uk>
2019-12-31 13:05:53 +0000

    perf: remove inessential packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,12 @@
-// const IN_PRODUCTION = process.env.NODE_ENV === 'production'
+const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  plugins: [
-    require('postcss-preset-env')({ stage: 0 }),
-    require('postcss-nested')({}),
-    require('tailwindcss')(),
-    // IN_PRODUCTION &&
-    //   require('@fullhuman/postcss-purgecss')({
-    //     content: [`./public/**/*.html`, `./src/**/*.vue`],
-    //     defaultExtractor(content) {
-    //       const contentWithoutStyleBlocks = content.replace(
-    //         /<style[^]+?<\/style>/gi,
-    //         '',
-    //       )
-    //       return (
-    //         contentWithoutStyleBlocks.match(
-    //           /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g,
-    //         ) || []
-    //       )
-    //     },
-    //     whitelist: [],
-    //     whitelistPatterns: [
-    //       /-(leave|enter|appear)(|-(to|from|active))$/,
-    //       /^(?!(|.*?:)cursor-move).+-move$/,
-    //       /^router-link(|-exact)-active$/,
-    //     ],
-    //   }),
-    require('autoprefixer')(),
-  ],
+  plugins: IN_PRODUCTION
+    ? []
+    : [
+        require('postcss-preset-env')({ stage: 0 }),
+        require('postcss-nested')({}),
+        require('tailwindcss')(),
+        require('autoprefixer')(),
+      ],
 }
