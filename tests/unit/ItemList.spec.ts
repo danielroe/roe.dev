a08f3243c75d6bc0871ea4be4a07c7208090124c
Daniel Roe <daniel@concision.co.uk>
2020-01-11 10:07:06 +0000

    test: add simple test for item list

---
+++ tests/unit/ItemList.spec.ts
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils'
+import ItemList from '@/components/ItemList.vue'
+
+describe('ItemList.vue', () => {
+  it('renders slot', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(ItemList, {
+      slots: {
+        default: msg,
+      },
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
