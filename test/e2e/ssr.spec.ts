ea622a5f0c3905b6933a833a6890d55caa0017c5
Daniel Roe <daniel@roe.dev>
2022-07-13 00:16:11 +0100

    test: add basic e2e test suite on deployment (#714)

---
+++ test/e2e/ssr.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test'
+import { joinURL } from 'ufo'
+
+const baseURL = process.env.BASE_URL || 'https://roe.dev/'
+
+const url = (path: string) => joinURL(baseURL, path)
+
+const pages = [
+  '/',
+  '/talks',
+  '/uses',
+  '/work',
+  '/blog',
+  '/blog/introduction',
+  '/blog/serverless-functions-nuxt-zeit-now',
+  '/blog/good-ux-and-giving-birth',
+  '/blog/building-your-own-vue-rich-text-component',
+]
+
+test.describe(`pages`, () => {
+  for (const path of pages) {
+    test(`renders ${path}`, async ({ page }) => {
+      await page.goto(url(path))
+
+      const title = page.locator('title')
+      expect(await title.textContent()).toContain('Daniel Roe')
+
+      await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.05 })
+    })
+
+    test(`image for ${path}`, async ({ page }) => {
+      await page.goto(url(path))
+
+      const ogImage = page.locator('[property="og:image"]')
+      const ogURL = await ogImage.getAttribute('content')
+      await page.goto(ogURL)
+      await expect(page).toHaveScreenshot()
+    })
+  }
+})
