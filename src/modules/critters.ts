31fc0fc2b774e91864c629aa574c662e11292d53
Daniel Roe <daniel@roe.dev>
2022-07-13 22:42:50 +0100

    perf: use critters to inline style when building

---
+++ src/modules/critters.ts
@@ -0,0 +1,35 @@
+import { defineNuxtModule } from '@nuxt/kit'
+import Critters, { Options } from 'critters'
+
+export interface ModuleOptions {
+  config?: Options
+}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'critters',
+    configKey: 'critters',
+  },
+  defaults: {
+    // Options passed directly to `critters`
+    config: {
+      preload: 'media',
+    },
+  },
+  setup(options, nuxt) {
+    // Only enable for production
+    if (nuxt.options.dev) return
+
+    nuxt.hook('nitro:init', nitro => {
+      const critters = new Critters({
+        path: nitro.options.output.publicDir,
+        publicPath: nuxt.options.app.baseURL,
+        ...options.config,
+      })
+      nitro.hooks.hook('prerender:route', async route => {
+        if (!route.contents) return
+        route.contents = await critters.process(route.contents)
+      })
+    })
+  },
+})
