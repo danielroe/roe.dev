4d890e56beff75b2388bb0da5e2871063162a1ce
Daniel Roe <daniel@roe.dev>
2022-08-06 15:05:07 +0100

    refactor: extract components chunking into module

---
+++ src/modules/components-chunk.ts
@@ -0,0 +1,42 @@
+import { defineNuxtModule, useNuxt } from '@nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'components-chunk',
+  },
+  setup() {
+    const nuxt = useNuxt()
+    let dirs: string[]
+
+    nuxt.hook('components:dirs', _dirs => {
+      dirs = _dirs.map(d => (typeof d === 'string' ? d : d.path))
+    })
+
+    // Exclude non-prose content components
+    nuxt.hook('components:extend', components => {
+      for (const component of components) {
+        if (
+          'filePath' in component &&
+          component.filePath.includes('@nuxt/content')
+        ) {
+          if (!component.filePath.includes('/Prose/')) {
+            components.splice(components.indexOf(component), 1)
+          }
+        }
+      }
+    })
+
+    // Use single components chunk
+    nuxt.hook('vite:extendConfig', config => {
+      if (Array.isArray(config.build.rollupOptions.output)) return
+      config.build.rollupOptions.output.manualChunks = id => {
+        if (
+          id.includes('@nuxt/content') &&
+          dirs.some(dir => id.includes(dir))
+        ) {
+          return 'components-chunk'
+        }
+      }
+    })
+  },
+})
