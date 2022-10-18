6900b599d1158eb8e1d5936a83dc16cf430081f7
Daniel Roe <daniel@roe.dev>
2022-10-18 21:43:50 +0100

    style: single line

--- src/server/plugins/sprite.ts
+++ src/server/plugins/sprite.ts
@@ -1,9 +1,6 @@
 const sprite =
   /* svg */ `<svg class="hidden" xmlns="http://www.w3.org/2000/svg">
-  <symbol
-    id="loading"
-    viewBox="0 0 24 24"
-  >
+  <symbol id="loading" viewBox="0 0 24 24">
     <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-dasharray="60" stroke-dashoffset="60" stroke-opacity=".3" d="M12 3a9 9 0 1 1 0 18 9 9 0 0 1 0-18Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"/></path><path stroke-dasharray="15" stroke-dashoffset="15" d="M12 3a9 9 0 0 1 9 9"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></g>
   </symbol>
   <symbol id="github" viewBox="0 0 128 128">
