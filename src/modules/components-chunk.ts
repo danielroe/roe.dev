219e1922307cfbb23440e489253cdd7482a61140
Daniel Roe <daniel@roe.dev>
2022-10-19 02:01:27 +0100

    perf: exclude unused components from build (#789)

--- src/modules/components-chunk.ts
+++ src/modules/components-chunk.ts
@@ -4,7 +4,7 @@ export default defineNuxtModule({
   meta: {
     name: 'components-chunk',
   },
-  setup() {
+  setup () {
     const nuxt = useNuxt()
     let dirs: string[]
 
@@ -12,17 +12,39 @@ export default defineNuxtModule({
       dirs = _dirs.map(d => (typeof d === 'string' ? d : d.path))
     })
 
+    const usedComponents = [
+      'ContentRendererMarkdown',
+      'ProseP',
+      'ProseA',
+      'ProseH3',
+      'ProseLi',
+      'ProseOl',
+      'ProseUl',
+      'ProseThead',
+      'ProseTable',
+      'ProseTr',
+      'ProseTd',
+      'ProseTh',
+      'ProseTbody',
+      'ProseCode',
+      'ProseCodeInline',
+    ]
+
     // Exclude non-prose content components
     nuxt.hook('components:extend', components => {
+      const toPurge = []
       for (const component of components) {
         if (
           'filePath' in component &&
           component.filePath.includes('@nuxt/content')
         ) {
-          if (!component.filePath.includes('/Prose/')) {
-            components.splice(components.indexOf(component), 1)
+          if (!usedComponents.includes(component.pascalName)) {
+            toPurge.push(component)
           }
         }
+      }
+      for (const component of toPurge) {
+        components.splice(components.indexOf(component), 1)
       }
     })
 
