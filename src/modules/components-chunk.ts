852c590633bff7a80122add1e014810b76be42d1
Daniel Roe <daniel@roe.dev>
2022-10-19 01:56:01 +0100

    revert: return to previous chunk behaviour

--- src/modules/components-chunk.ts
+++ src/modules/components-chunk.ts
@@ -12,19 +12,39 @@ export default defineNuxtModule({
       dirs = _dirs.map(d => (typeof d === 'string' ? d : d.path))
     })
 
-    const usedComponents = ['ContentRendererMarkdown']
+    const usedComponents = [
+      'ContentRendererMarkdown',
+      'ProseP',
+      'ProseA',
+      'ProseH3',
+      'ProseLi',
+      'ProseOl',
+      'ProseUl',
+      'ProseThead',
+      'ProseTable',
+      'ProseTr',
+      'ProseTd',
+      'ProseTh',
+      'ProseTbody',
+      'ProseCode',
+      'ProseCodeInline',
+    ]
 
     // Exclude non-prose content components
     nuxt.hook('components:extend', components => {
+      const toPurge = []
       for (const component of components) {
         if (
           'filePath' in component &&
           component.filePath.includes('@nuxt/content')
         ) {
           if (!usedComponents.includes(component.pascalName)) {
-            component.global = false
+            toPurge.push(component)
           }
         }
+      }
+      for (const component of toPurge) {
+        components.splice(components.indexOf(component), 1)
       }
     })
 
