7f98eedc48a76d2ab45b196c4c9765c261308bd8
Daniel Roe <daniel@roe.dev>
2023-05-20 18:26:42 +0100

    fix: revert client/server separation for footer (for moment)

---
+++ test/unit/behaviour.spec.ts
@@ -0,0 +1,19 @@
+/** @vitest-environment node */
+import { fileURLToPath } from 'node:url'
+
+import { describe, expect, it } from 'vitest'
+import { createPage, setup } from '@nuxt/test-utils'
+
+await setup({
+  rootDir: fileURLToPath(new URL('../..', import.meta.url)),
+})
+
+describe('site behaviour', () => {
+  it('renders server components', async () => {
+    const page = await createPage('/')
+    await page.waitForLoadState('networkidle')
+    expect(
+      await page.getByText('All rights reserved').innerHTML()
+    ).toMatchInlineSnapshot('"© 2019-2023 Daniel Roe. All rights reserved."')
+  })
+})
