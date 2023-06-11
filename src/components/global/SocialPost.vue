0eacd163eb12b4717fd0e6f4f782e9da3cc77ab9
Daniel Roe <daniel@roe.dev>
2023-06-11 21:51:58 +0100

    feat: add social icon to tweet

--- src/components/global/SocialPost.vue
+++ src/components/global/SocialPost.vue
@@ -2,6 +2,7 @@
 defineProps({
   link: String,
   datetime: String,
+  social: String,
 })
 </script>
 
@@ -11,19 +12,24 @@ defineProps({
       <slot />
     </div>
     <div
-      v-if="datetime || link"
-      class="flex flex-row justify-between mt-6 pb-0 !mb-2"
+      v-if="datetime || link || social"
+      class="flex flex-row gap-2 w-full mt-6 pb-0 !mb-2"
     >
-      <NuxtTime
-        v-if="datetime"
-        :datetime="datetime"
-        weekday="long"
-        day="numeric"
-        month="long"
-        year="numeric"
-      />
-      <div v-else />
-      <a v-if="link" class="self-end" :href="link">Link</a>
+      <div v-if="social" class="max-w-6">
+        <svg class="inline-block h-4 w-4 fill-current" alt="">
+          <use :xlink:href="`#${social}`" />
+        </svg>
+      </div>
+      <component :is="link ? 'a' : 'div'" :href="link">
+        <NuxtTime
+          v-if="datetime"
+          :datetime="datetime"
+          weekday="long"
+          day="numeric"
+          month="long"
+          year="numeric"
+        />
+      </component>
     </div>
   </div>
 </template>
