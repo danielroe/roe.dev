962654bf09afdb9f2ef6c731d7f1df661365c4f1
Daniel Roe <daniel@roe.dev>
2023-06-12 09:27:44 +0100

    fix: remove query string from avatar urls

--- src/server/api/sponsors.get.ts
+++ src/server/api/sponsors.get.ts
@@ -3,7 +3,9 @@ import { getSponsors } from '../utils/sponsors'
 export default defineCachedEventHandler(
   async () => {
     const sponsors = await getSponsors()
-    return sponsors.map(s => s.avatarUrl).filter((r): r is string => !!r)
+    return sponsors
+      .map(s => s.avatarUrl?.replace(/(\?|%3Fu).*$/, ''))
+      .filter((r): r is string => !!r)
   },
   { maxAge: 60 }
 )
