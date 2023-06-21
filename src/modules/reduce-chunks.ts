716aca0444963f0857a0bfc6d6c68da78b6ffdec
Daniel Roe <daniel@roe.dev>
2023-06-21 11:04:28 +0100

    fix: remove unneeded error chunk

--- src/modules/reduce-chunks.ts
+++ src/modules/reduce-chunks.ts
@@ -1,24 +1,13 @@
-import { addVitePlugin, defineNuxtModule, useNuxt } from '@nuxt/kit'
+import { defineNuxtModule, useNuxt } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: 'reduce-chunks',
   },
-  setup () {
+  setup() {
     const nuxt = useNuxt()
     nuxt.hook('app:resolve', app => {
       app.plugins = app.plugins.filter(p => !p.src.includes('/ws.mjs'))
-    })
-    addVitePlugin({
-      name: 'reduce-chunks',
-      enforce: 'pre',
-      transform (code, id) {
-        if (id.includes('nuxt-root.vue'))
-          return code.replace(
-            `const ErrorComponent = defineAsyncComponent(() => import('#build/error-component.mjs').then(r => r.default || r))`,
-            "import ErrorComponent from '#build/error-component.mjs'"
-          )
-      },
     })
   },
 })
