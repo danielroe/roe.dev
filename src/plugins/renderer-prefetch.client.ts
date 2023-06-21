2fc7e5f57ff30082806500fd7bd9be438475a87d
Daniel Roe <daniel@roe.dev>
2023-06-21 11:04:27 +0100

    perf: remove ContentRendererMarkdown prefetch

--- src/plugins/renderer-prefetch.client.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.hook('app:suspense:resolve', () => {
-    requestIdleCallback(() => {
-      prefetchComponents(['ContentRendererMarkdown'])
-    })
-  })
-})
