57c01be966a3ecd7b2c4897992a4a8151af44659
Daniel Roe <daniel@roe.dev>
2023-06-21 11:01:46 +0100

    fix: move some styles into classess

--- src/components/GridLink.vue
+++ src/components/GridLink.vue
@@ -1,20 +1,7 @@
 <template>
   <NuxtLink
-    :class="$style.item"
-    class="p-4 relative text-xl flex flex-col justify-end shadow hover:outline-none active:outline-none focus:outline-none min-h-12 transition-all border-[1px] border-solid border-transparent after:text-transparent flex-[100%]"
+    class="p-4 relative text-xl flex flex-col justify-end shadow hover:outline-none active:outline-none focus:outline-none min-h-12 transition-all border-[1px] border-solid border-transparent after:text-transparent flex-[100%] bg-[var(--accent)] hover:border-[var(--text-muted)] active:border-[var(--text-muted)] focus:border-[var(--text-muted)]"
   >
     <slot />
   </NuxtLink>
 </template>
-
-<style module>
-.item {
-  background-color: var(--accent, theme('colors.gray.900'));
-
-  &:hover,
-  &:active,
-  &:focus {
-    border-color: var(--text-muted);
-  }
-}
-</style>
