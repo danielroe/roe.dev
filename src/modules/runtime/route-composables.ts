325c7cdcdcaa9e2208859fe60236470ebc3d45fa
Daniel Roe <daniel@roe.dev>
2023-07-03 23:04:56 +0100

    fix: return _route_

--- src/modules/runtime/route-composables.ts
+++ src/modules/runtime/route-composables.ts
@@ -1,7 +1,4 @@
-import type { Router } from '#vue-router'
-
 /*! @__NO_SIDE_EFFECTS__ */
 export const definePageMeta = (_args: any) => {}
 
-export const useRoute = (_path?: string) =>
-  useNuxtApp().$router as any as Router
+export const useRoute = (_path?: string) => useNuxtApp().$route
