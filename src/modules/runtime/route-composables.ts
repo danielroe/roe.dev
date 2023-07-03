9fc473a0b6f94d85942553fa05d3f91b56a75a8f
Daniel Roe <daniel@roe.dev>
2023-07-03 23:02:46 +0100

    chore: fix type

--- src/modules/runtime/route-composables.ts
+++ src/modules/runtime/route-composables.ts
@@ -1,4 +1,7 @@
+import type { Router } from '#vue-router'
+
 /*! @__NO_SIDE_EFFECTS__ */
 export const definePageMeta = (_args: any) => {}
 
-export const useRoute = (_path?: string) => useNuxtApp().$router
+export const useRoute = (_path?: string) =>
+  useNuxtApp().$router as any as Router
