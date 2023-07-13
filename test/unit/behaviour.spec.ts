5b2558428157bc6440d325442b0d25101843320c
Daniel Roe <daniel@roe.dev>
2023-07-13 13:27:24 +0100

    test: extend test timeout and assert no error logs

--- test/unit/behaviour.spec.ts
+++ test/unit/behaviour.spec.ts
@@ -2,7 +2,7 @@
 import { fileURLToPath } from 'node:url'
 
 import { describe, expect, it } from 'vitest'
-import { createPage, setup } from '@nuxt/test-utils'
+import { createPage, setup, url } from '@nuxt/test-utils'
 
 await setup({
   rootDir: fileURLToPath(new URL('../..', import.meta.url)),
@@ -21,12 +21,26 @@ await setup({
   },
 })
 
-describe('site behaviour', () => {
-  it('renders server components', async () => {
-    const page = await createPage('/')
-    await page.waitForLoadState('networkidle')
-    expect(await page.getByText('2019-2023').innerHTML()).toMatchInlineSnapshot(
-      '" © 2019-2023 Daniel Roe. <a class=\\"link\\" href=\\"https://creativecommons.org/licenses/by-nc-sa/4.0/\\"> CC BY-SA 4.0 </a>"'
-    )
-  })
-})
+describe(
+  'site behaviour',
+  () => {
+    it('renders server components', async () => {
+      const logs: string[] = []
+      const page = await createPage()
+      page.on('console', msg => logs.push(msg.text()))
+      await page.goto(url('/'))
+      await page.waitForLoadState('networkidle')
+      expect(
+        await page.getByText('2019-2023').innerHTML()
+      ).toMatchInlineSnapshot(
+        '" © 2019-2023 Daniel Roe. <a class=\\"link\\" href=\\"https://creativecommons.org/licenses/by-nc-sa/4.0/\\"> CC BY-SA 4.0 </a>"'
+      )
+      expect(logs).toMatchInlineSnapshot(`
+        [
+          "[Plausible] Ignoring event because website is running locally",
+        ]
+      `)
+    })
+  },
+  { timeout: 10000 }
+)
