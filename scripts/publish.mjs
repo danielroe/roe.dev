dd0312e7604b7d9ef8846222c0e498c7344c8401
Daniel Roe <daniel@roe.dev>
2023-07-13 13:22:50 +0100

    refactor: rewrite publish script into single file

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,101 +1,99 @@
 /* eslint-disable camelcase */
 
+import fsp from 'node:fs/promises'
+import { globby } from 'globby'
 import { $fetch } from 'ofetch'
 
-import { iterateOnDirectory } from './global.mjs'
-
-const url = 'https://dev.to/api'
 const token = process.env.DEVTO_TOKEN
+
+if (!token) {
+  throw new Error('No token provided.')
+}
+
+const $devto = $fetch.create({
+  baseURL: 'https://dev.to/api',
+  headers: {
+    'api-key': token,
+  },
+})
+
+const serializers = new Map([
+  [/\(\//g, '(https://roe.dev/'],
+  [/ ---? /g, ' — '],
+  [/::SocialPost\{link=([^ ]*)[\s\S]*?::/g, '$1'],
+  [/:CalSchedule\{meeting=([^ ]*).*\}/g, 'https://cal.com/danielroe/$1'],
+  [/(\n|^)---\n[\s\S]*\n---\n/, '\n'],
+])
 
 async function getMarkdownArticles() {
   const articles = []
-  await iterateOnDirectory('./src/content/blog', (path, contents) => {
-    if (!/\.md$/.test(path)) return
-    if (contents.includes('skip_dev')) return
-    const slug = path.match(/\/[^/]*$/)?.[0].slice(1, -3)
+  const files = await globby('./src/content/blog/**/*.md')
+  for (const file of files) {
+    let contents = await fsp.readFile(file, 'utf-8')
+
+    if (contents.includes('skip_dev')) continue
+    for (const item of serializers) {
+      contents = contents.replace(item[0], item[1])
+    }
+
+    const slug = file.match(/\/[^/]*$/)?.[0].slice(1, -3)
     articles.push({
-      body_markdown: contents
-        .replace(/\(\//g, '(https://roe.dev/')
-        .replace(/ ---? /g, ' — ')
-        .replace(/::SocialPost\{link=([^ ]*)[\s\S]*?::/g, '$1')
-        .replace(
-          /:CalSchedule\{meeting=([^ ]*).*\}/g,
-          'https://cal.com/danielroe/$1'
-        )
-        .replace(/(\n|^)---\n[\s\S]*\n---\n/, '\n'),
+      body_markdown: contents,
       title: contents.match(/title: (.*)/)?.[1],
       slug,
       canonical_url: `https://roe.dev/blog/${slug}/`,
     })
-  })
+  }
   return articles
 }
 
 function getArticles() {
-  if (!token) {
-    throw new Error('No token provided')
-  }
-  return $fetch(`${url}/articles/me`, {
-    headers: {
-      'api-key': token,
+  return $devto('articles/me')
+}
+
+async function postArticle({ title, body_markdown, canonical_url }) {
+  return await $devto('articles', {
+    method: 'POST',
+    body: {
+      article: {
+        published: true,
+        title,
+        canonical_url,
+        body_markdown,
+      },
     },
   })
 }
 
-async function postArticle({ title, body_markdown, canonical_url }) {
-  try {
-    return await $fetch(`${url}/articles`, {
-      method: 'POST',
-      body: {
-        article: {
-          published: true,
-          title,
-          canonical_url,
-          body_markdown,
-        },
-      },
-      headers: {
-        'api-key': token,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-  }
-}
-
 async function updateArticle(id, { title, body_markdown, canonical_url }) {
-  try {
-    return await $fetch(`${url}/articles/${id}`, {
-      method: 'PUT',
-      body: {
-        article: {
-          published: true,
-          title,
-          body_markdown,
-          canonical_url,
-        },
+  return await $devto(`articles/${id}`, {
+    method: 'PUT',
+    body: {
+      article: {
+        published: true,
+        title,
+        body_markdown,
+        canonical_url,
       },
-      headers: {
-        'api-key': token,
-      },
-    })
-  } catch (e) {
-    console.log(e)
-  }
+    },
+  })
 }
 
-getArticles().then(async articles => {
-  const markdownArticles = await getMarkdownArticles()
-  // console.log('TCL: markdownArticles', markdownArticles)
-  for (const markdownArticle of markdownArticles) {
-    const article = articles.find(
+async function main() {
+  const [publishedArticles, localArticles] = await Promise.all([
+    getArticles(),
+    getMarkdownArticles(),
+  ])
+  for (const markdownArticle of localArticles) {
+    const article = publishedArticles.find(
       article => article.canonical_url === markdownArticle.canonical_url
     )
-    // console.log('TCL: article', article)
     if (article) {
       await updateArticle(article.id, markdownArticle)
     } else {
       await postArticle(markdownArticle)
     }
   }
-})
+}
+
+main()
