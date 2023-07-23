aef0be0708342668942aca35f5a56d484d608f81
Daniel Roe <daniel@roe.dev>
2023-07-23 09:12:47 +0100

    chore: bump various modules to handle node16 options (#1264)

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -83,7 +83,7 @@ describe('project sizes', () => {
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot('"27967k"')
+      .toMatchInlineSnapshot('"27574k"')
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
