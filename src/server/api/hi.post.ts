c2319318c06ae80c237abe574718cb2c85591f56
Daniel Roe <daniel@roe.dev>
2023-07-27 09:30:40 +0200

    fix: add cors headers

--- src/server/api/hi.post.ts
+++ src/server/api/hi.post.ts
@@ -1,4 +1,8 @@
 export default defineEventHandler(async event => {
+  handleCors(event, {})
+
+  if (getMethod(event) === 'OPTIONS') return null
+
   const { body } = await readBody(event)
   if (!body) throw createError({ statusCode: 422 })
 
