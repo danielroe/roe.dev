90971fa070df5dbfb2d2f689e63c7a5bfee939d0
Daniel Roe <daniel@roe.dev>
2023-09-22 11:06:58 +0200

    chore: update nuxt to v3.7.3 (#1296)

--- src/composables/onIdle.ts
+++ src/composables/onIdle.ts
@@ -1,24 +1,3 @@
-export const requestIdleCallback =
-  globalThis.requestIdleCallback ||
-  function (cb) {
-    const start = Date.now()
-    const idleDeadline = {
-      didTimeout: false,
-      timeRemaining() {
-        return Math.max(0, 50 - (Date.now() - start))
-      },
-    }
-    return setTimeout(function () {
-      cb(idleDeadline)
-    }, 1)
-  }
-
-export const cancelIdleCallback =
-  globalThis.cancelIdleCallback ||
-  function (id) {
-    clearTimeout(id)
-  }
-
 export const onIdle = (fn: () => any) => {
   let id: number
   onBeforeMount(() => {
