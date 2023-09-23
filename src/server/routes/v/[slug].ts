98f2a955282497f59c41813c149914423fb820f4
Daniel Roe <daniel@roe.dev>
2023-09-23 12:47:14 +0200

    fix: accept number vote slugs 🙈

--- src/server/routes/v/[slug].ts
+++ src/server/routes/v/[slug].ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async event => {
   const config = useRuntimeConfig()
   const slug = getRouterParam(event, 'slug')
-  if (!slug || !/^[a-z]+$/.test(slug)) {
+  if (!slug || !/^[\da-z]+$/.test(slug)) {
     throw createError({ statusCode: 400, statusMessage: 'Missing slug' })
   }
   await $fetch(slug, { baseURL: config.voteUrl, method: 'POST' })
