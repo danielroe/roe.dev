1f7cda22ef44fc104e8922b722535a751acfb171
Daniel Roe <daniel@roe.dev>
2023-10-25 20:41:49 +0900

    chore: remove invite link

--- src/modules/invites.ts
+++ src/modules/invites.ts
@@ -12,7 +12,7 @@ export default defineNuxtModule({
   },
   defaults: {
     map: {
-      zen: 'danielroe/nailing-it-japan-2023',
+      // zen: 'danielroe/nailing-it-japan-2023',
       // vueconf: 'danielroe/nailing-it-vueconf-us-2023',
     } satisfies Record<string, string>,
   },
