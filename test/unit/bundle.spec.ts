d5da4a5554925c3aa8c8d308fd2f827c3aa8be45
Daniel Roe <daniel@roe.dev>
2023-12-25 16:41:23 +0000

    test: update snapshots

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"214k"`)
+      .toMatchInlineSnapshot(`"211k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js')))
       .toMatchInlineSnapshot(`
         [
@@ -88,12 +88,12 @@ describe('project sizes', () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
     expect
       .soft(roundToKilobytes(stats.server.totalBytes))
-      .toMatchInlineSnapshot(`"414k"`)
+      .toMatchInlineSnapshot(`"417k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot('"32384k"')
+      .toMatchInlineSnapshot(`"31913k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
