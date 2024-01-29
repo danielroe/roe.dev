fb5abda43fd63e02f44f7aee32b6142ad7cbee5b
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-01-29 17:29:49 +0100

    fix: avoid flash or non-responsiveness on color mode toggle (#1360)

--- src/components/ToggleColorMode.vue
+++ src/components/ToggleColorMode.vue
@@ -8,17 +8,10 @@ const colorMode = useColorMode()
     aria-label="Change site theme"
     type="button"
     class="h-7 w-12 relative rounded-full bg-accent ml-2 items-center justify-start px-1 transition-opacity outline-none border-2 border-solid border-accent hover:border-primary focus:border-primary active:border-primary"
-    @click="
-      colorMode.value === 'dark'
-        ? (colorMode.preference = 'light')
-        : (colorMode.preference = 'dark')
-    "
+    @click="colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'"
   >
     <span
-      class="inline-block rounded-full h-4 w-4 bg-primary transition-transform"
-      :style="
-        colorMode.value === 'light' ? 'transform:translateX(1.25rem)' : ''
-      "
+      class="inline-block rounded-full h-4 w-4 bg-primary transition-transform light:translate-x-5"
     />
   </button>
 </template>
