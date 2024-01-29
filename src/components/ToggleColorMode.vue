2d890e366c2e83d00f1cfb1151d4b2b20f52388c
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-01-29 15:15:24 +0100

    refactor: update logic for toggling color theme in ToggleColorMode.vue

--- src/components/ToggleColorMode.vue
+++ src/components/ToggleColorMode.vue
@@ -4,11 +4,7 @@
       aria-label="Change site theme"
       type="button"
       class="h-7 w-12 relative rounded-full bg-accent ml-2 items-center justify-start px-1 transition-opacity outline-none border-2 border-solid border-accent hover:border-primary focus:border-primary active:border-primary"
-      @click="
-        $colorMode.value === 'dark'
-          ? ($colorMode.preference = 'light')
-          : ($colorMode.preference = 'dark')
-      "
+      @click="$colorMode.preference = $colorMode.value === 'dark' ? 'light' : 'dark'"
     >
       <span
         :class="['absolute -top-[50%] translate-y-[100%] inline-block rounded-full h-4 w-4 bg-primary transition-transform', $colorMode.value === 'dark' ? '-translate-x-4.5': 'translate-x-0.5']"
