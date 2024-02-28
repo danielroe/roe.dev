dff582572623a1a49d6e8d1afa2ed602f5fa5a6c
Daniel Roe <daniel@roe.dev>
2024-02-28 12:45:33 +0100

    fix: add label for textarea

--- src/pages/feedback.vue
+++ src/pages/feedback.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="flex-grow px-4 py-2 md:px-12 md:py-4 w-full">
-    <header class="leading-none mt-[5vw] mb-[1vw] max-w-[37.50rem]">
-      <h1 class="text-2xl">
-        What do you think of Nuxt, in one word or one sentence?
-      </h1>
-    </header>
     <main class="text-muted text-lg">
       <form
         class="flex flex-col items-start py-8 gap-2"
         @submit.prevent="submitFeedback"
       >
+        <label
+          for="feedback"
+          class="text-2xl leading-none mt-[5vw] mb-[1vw] max-w-[37.50rem]"
+        >
+          What do you think of Nuxt, in one word or one sentence?
+        </label>
         <textarea
+          id="feedback"
           name="feedback"
           class="rounded w-full max-w-400px text-black px-3 py-1"
         />
