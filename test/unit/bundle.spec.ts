7f74f4ab4a007494e6e3b74ac235b8ee047ed205
Daniel Roe <daniel@roe.dev>
2024-02-29 12:35:38 +0100

    feat: move to `@nuxt/fonts` (#1376)

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"215k"`)
+      .toMatchInlineSnapshot(`"216k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js')))
       .toMatchInlineSnapshot(`
         [
@@ -79,7 +79,7 @@ describe('project sizes', () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
     expect
       .soft(roundToKilobytes(stats.server.totalBytes))
-      .toMatchInlineSnapshot(`"1038k"`)
+      .toMatchInlineSnapshot(`"1044k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
