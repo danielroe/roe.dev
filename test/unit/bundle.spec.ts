96460b5504a6c442e8cbdddd19afbc9e1b9bcbc3
Daniel Roe <daniel@roe.dev>
2024-03-07 14:27:42 +0000

    chore: upgrade dependencies (#1384)

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -84,7 +84,7 @@ describe('project sizes', () => {
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot(`"5296k"`)
+      .toMatchInlineSnapshot(`"5348k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
