5a4d4a01d980a64705e8da0f30f975cd57f5fdbe
Daniel Roe <daniel@roe.dev>
2024-04-05 11:05:55 +0100

    test: close page

--- test/unit/behaviour.spec.ts
+++ test/unit/behaviour.spec.ts
@@ -21,28 +21,23 @@ await setup({
   },
 })
 
-describe(
-  'site behaviour',
-  { timeout: 10000 },
-  () => {
-    it('renders server components', async () => {
-      const logs: string[] = []
-      const page = await createPage()
-      await page.route('**/feed/_payload.json', route =>
-        route.fulfill({
-          json: [{ data: 2, prerenderedAt: 3 }, {}, 1689517334625],
-        }),
-      )
-      page.on('console', msg => logs.push(msg.text()))
-      await page.goto(url('/'))
-      await page.waitForLoadState('networkidle')
-      const year = new Date().getFullYear()
-      expect(
-        await page.getByText(`2019-${year}`).innerHTML(),
-      ).toMatchInlineSnapshot(
-        `" © 2019-${year} Daniel Roe. <a class="underlined-link" href="https://creativecommons.org/licenses/by-sa/4.0/"> CC BY-SA 4.0 </a>"`,
-      )
-      expect(logs).toMatchInlineSnapshot(`[]`)
-    })
-  },
-)
+describe('site behaviour', { timeout: 10000 }, () => {
+  it('renders server components', async () => {
+    const logs: string[] = []
+    const page = await createPage()
+    await page.route('**/feed/_payload.json', route =>
+      route.fulfill({
+        json: [{ data: 2, prerenderedAt: 3 }, {}, 1689517334625],
+      }),
+    )
+    page.on('console', msg => logs.push(msg.text()))
+    await page.goto(url('/'))
+    await page.waitForLoadState('networkidle')
+    const year = new Date().getFullYear()
+    expect(await page.getByText(`2019-${year}`).innerHTML()).toMatchInlineSnapshot(
+      `" © 2019-${year} Daniel Roe. <a class="underlined-link" href="https://creativecommons.org/licenses/by-sa/4.0/"> CC BY-SA 4.0 </a>"`,
+    )
+    expect(logs).toMatchInlineSnapshot(`[]`)
+    await page.close()
+  })
+})
