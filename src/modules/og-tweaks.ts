07c98127623d11816c96dc558addb16777199e44
Daniel Roe <daniel@roe.dev>
2024-04-09 19:30:39 +0200

    perf: purge og-image endpoints

--- src/modules/og-tweaks.ts
+++ src/modules/og-tweaks.ts
@@ -1,4 +1,5 @@
 import { defineNuxtModule, useNuxt } from 'nuxt/kit'
+import type { InputPluginOption } from 'rollup'
 
 export default defineNuxtModule({
   meta: {
@@ -18,6 +19,24 @@ export default defineNuxtModule({
     // Prevent installing runtime nitro plugin to handle content hooks + SPA site config
     nuxt.hook('modules:done', () => {
       nuxt.options.nitro.plugins = nuxt.options.nitro.plugins?.filter(p => p && !p.includes('runtime/nitro/plugins/nuxt-content') && !p.includes('runtime/nitro/plugins/injectState')) || []
+    })
+
+    // Purge OG image endpoints for runtime (as they are fully prerendered)
+    nuxt.hook('nitro:config', config => {
+      (config.rollupConfig!.plugins as InputPluginOption[]).push({
+        name: 'purge-the-handler',
+        transform (_code, id) {
+          if (id.includes('__og-image__')) {
+            return 'export default defineEventHandler(() => {})'
+          }
+        },
+      })
+    })
+
+    nuxt.hook('nitro:init', nitro => {
+      nitro.options._config.rollupConfig!.plugins = (
+        nitro.options._config.rollupConfig!.plugins as InputPluginOption[]
+      ).filter(p => !p || !('name' in p) || p.name !== 'purge-the-handler')
     })
   },
 })
