50eb6ce225aeb64926decb3465fe23e471286822
Daniel Roe <daniel@roe.dev>
2024-04-18 12:17:55 +0300

    feat: add slides for devoxx greece

--- src/server/api/upcoming-conferences.get.ts
+++ src/server/api/upcoming-conferences.get.ts
@@ -12,17 +12,6 @@ const upcomingConferences: Array<{
   location: string
 }> = [
   {
-    name: 'Devoxx Greece',
-    dates: '18-20 April',
-    link: 'https://devoxx.gr/',
-    location: '🇬🇷',
-    image: {
-      url: 'https://devoxx.gr/wp-content/uploads/2024/01/devoxxgreece.png',
-      height: 630,
-      width: 945,
-    },
-  },
-  {
     name: 'Vue.js Live',
     dates: '25-26 April',
     link: 'https://vuejslive.com/',
