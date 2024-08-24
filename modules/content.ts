3e5d365047af1ed4cc6b92fe4960eb650c010537
Daniel Roe <daniel@roe.dev>
2024-08-24 21:15:54 +0100

    perf: only include content endpoints for prerender

---
+++ modules/content.ts
@@ -0,0 +1,21 @@
+import { defineNuxtModule, useNuxt } from 'nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'nuxt-content-purge',
+  },
+  setup () {
+    const nuxt = useNuxt()
+    nuxt.hook('modules:done', () => {
+      const purgedHandlers: any[] = []
+      nuxt.hook('nitro:init', nitro => {
+        for (const handler of nitro.options.handlers!) {
+          if (handler?.route && /\/api\/_(content|mdc)/.test(handler.route)) {
+            purgedHandlers.push(handler)
+          }
+        }
+        nitro.options.handlers = nitro.options.handlers!.filter(handler => !purgedHandlers.includes(handler))
+      })
+    })
+  },
+})
