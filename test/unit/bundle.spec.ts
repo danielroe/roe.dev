83fa3885ee0ef327aba8d8bf083aed380554096c
Daniel Roe <daniel@roe.dev>
2024-09-04 11:13:04 +0100

    chore: bump nuxt version (#1440)

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"229k"`)
+      .toMatchInlineSnapshot(`"228k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js')))
       .toMatchInlineSnapshot(`
         [
@@ -84,7 +84,7 @@ describe('project sizes', () => {
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot(`"7468k"`)
+      .toMatchInlineSnapshot(`"7494k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
