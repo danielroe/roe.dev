a7d2a08848bebc7cbbef3fd0f42c5d865f5d56aa
Daniel Roe <daniel@roe.dev>
2024-09-12 22:39:52 +0100

    chore: bump mdc + unhead (#1446)

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"228k"`)
+      .toMatchInlineSnapshot(`"229k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js')))
       .toMatchInlineSnapshot(`
         [
@@ -84,7 +84,7 @@ describe('project sizes', () => {
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot(`"7580k"`)
+      .toMatchInlineSnapshot(`"7550k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
@@ -99,7 +99,11 @@ describe('project sizes', () => {
         "@atproto/xrpc",
         "@babel/parser",
         "@shikijs/core",
+        "@shikijs/engine-javascript",
+        "@shikijs/engine-oniguruma",
         "@shikijs/transformers",
+        "@shikijs/types",
+        "@shikijs/vscode-textmate",
         "@sindresorhus/is",
         "@ungap/structured-clone",
         "@unhead/dom",
@@ -142,8 +146,10 @@ describe('project sizes', () => {
         "hast-util-is-element",
         "hast-util-parse-selector",
         "hast-util-raw",
+        "hast-util-to-html",
         "hast-util-to-parse5",
         "hast-util-to-string",
+        "hast-util-whitespace",
         "hastscript",
         "hookable",
         "html-void-elements",
@@ -200,9 +206,11 @@ describe('project sizes', () => {
         "micromark-util-subtokenize",
         "multiformats",
         "node-emoji",
+        "oniguruma-to-js",
         "parse-entities",
         "parse5",
         "property-information",
+        "regex",
         "rehype-external-links",
         "rehype-raw",
         "rehype-sort-attribute-values",
