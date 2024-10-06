a87bc04f8711e8e2f1766267f0c3b47a96f4c5a3
Daniel Roe <daniel@roe.dev>
2024-10-06 21:10:57 +0100

    chore(deps): update all non-major dependencies (#1458)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"266k"`)
+      .toMatchInlineSnapshot(`"267k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js').replace(/_scripts\/.*\.js/, '_scripts/script.js')).sort())
       .toMatchInlineSnapshot(`
         [
@@ -85,7 +85,7 @@ describe('project sizes', () => {
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot(`"7567k"`)
+      .toMatchInlineSnapshot(`"7574k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
