3c4ea9cbb0552cb5b5b75d05816045f6c631fdea
daniel <daniel@roe.dev>
2024-10-21 19:56:39 +0000

    chore: add nixpack for idx support

---
+++ .idx/dev.nix
@@ -0,0 +1,46 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.corepack_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "dbaeumer.vscode-eslint"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["pnpm" "dev"];
+          manager = "web";
+          env = {
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        npm-install = "pnpm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
