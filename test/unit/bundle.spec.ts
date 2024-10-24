7a43e5e5116630c0d984d929e27817270b08cf4b
Daniel Roe <daniel@roe.dev>
2024-10-24 14:38:09 +0100

    chore: bump bytes

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -80,7 +80,7 @@ describe('project sizes', () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
     expect
       .soft(roundToKilobytes(stats.server.totalBytes))
-      .toMatchInlineSnapshot(`"425k"`)
+      .toMatchInlineSnapshot(`"426k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
