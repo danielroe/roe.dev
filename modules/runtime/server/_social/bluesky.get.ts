0f6482950f04058a4dc866f7a794c7c9fc39f034
Daniel Roe <daniel@roe.dev>
2024-10-24 14:17:39 +0100

    fix: handle facets at end of posts

--- modules/runtime/server/_social/bluesky.get.ts
+++ modules/runtime/server/_social/bluesky.get.ts
@@ -73,18 +73,13 @@ export default defineLazyEventHandler(async () => {
           const post = p.post.record as PostRecord
           const embed = p.post.embed as PostEmbed | undefined
           const text = new MagicString(post.text)
-          text.replaceAll(/[<>&]/g, r => ({
-            '<': '&lt;',
-            '>': '&gt;',
-            '&': '&amp;',
-          }[r]!))
           for (const facet of post.facets || []) {
             const startIndex = [...post.text].findIndex(
               (_, i) =>
                 Buffer.byteLength(post.text.slice(0, i + 1))
                 > facet.index.byteStart,
             )
-            const endIndex = [...post.text, 'z'].findIndex(
+            const endIndex = [...post.text].findIndex(
               (_, i) =>
                 Buffer.byteLength(post.text.slice(0, i + 1))
                 >= facet.index.byteEnd,
@@ -92,11 +87,17 @@ export default defineLazyEventHandler(async () => {
             for (const feature of facet.features) {
               if (feature.$type === 'app.bsky.richtext.facet#link') {
                 text.appendRight(startIndex, `<a href="${feature.uri}">`)
-                text.appendLeft(endIndex + 1, '</a>')
+                text.appendLeft(endIndex === -1 ? post.text.length : endIndex + 1, '</a>')
                 continue
               }
             }
           }
+
+          text.replaceAll(/[<>&]/g, r => ({
+            '<': '&lt;',
+            '>': '&gt;',
+            '&': '&amp;',
+          }[r]!))
 
           for (const match of post.text.matchAll(/\n\n/g)) {
             text.overwrite(match.index!, match.index! + 2, '</p><p>')
