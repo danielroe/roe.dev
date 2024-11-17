1990d56e119d1e269c6f9c4d61fcc02d765aa426
Daniel Roe <daniel@roe.dev>
2024-11-17 05:46:40 -0500

    fix: load more bluesky post + use public api

--- modules/runtime/server/_social/bluesky.get.ts
+++ modules/runtime/server/_social/bluesky.get.ts
@@ -1,70 +1,74 @@
 import { AppBskyEmbedImages, AppBskyFeedPost, AtpAgent } from '@atproto/api'
+import type { FeedViewPost } from '@atproto/api/dist/client/types/app/bsky/feed/defs'
 import MagicString from 'magic-string'
 
 export default defineEventHandler(async event => {
-  const agent = new AtpAgent({ service: 'https://bsky.social' })
-  const { identifier, password } = useRuntimeConfig(event).social.networks.bluesky
+  const agent = new AtpAgent({ service: 'https://public.api.bsky.app' })
+  const { identifier } = useRuntimeConfig(event).social.networks.bluesky
 
-  await agent.login({
-    identifier,
-    password,
-  })
-  const posts = await agent.getAuthorFeed({ actor: identifier })
-  return Promise.all(
-    posts.data.feed
-      .filter(p => AppBskyFeedPost.isRecord(p.post.record) && p.post.author.handle === identifier && !p.reply && (!p.post.embed || AppBskyEmbedImages.isMain(p.post.embed)))
-      .map(p => {
-        const post = p.post.record as AppBskyFeedPost.Record
-        const embed = AppBskyEmbedImages.isMain(p.post.embed) ? p.post.embed : undefined
-        const text = new MagicString(post.text)
-        for (const facet of post.facets || []) {
-          const startIndex = [...post.text].findIndex(
-            (_, i) =>
-              Buffer.byteLength(post.text.slice(0, i + 1))
-              > facet.index.byteStart,
-          )
-          const endIndex = [...post.text].findIndex(
-            (_, i) =>
-              Buffer.byteLength(post.text.slice(0, i + 1))
-              >= facet.index.byteEnd,
-          )
-          for (const feature of facet.features) {
-            if (feature.$type === 'app.bsky.richtext.facet#link') {
-              text.appendRight(startIndex, `<a href="${feature.uri}">`)
-              text.appendLeft(endIndex === -1 ? post.text.length : endIndex + 1, '</a>')
-              continue
-            }
-          }
+  const feed: FeedViewPost[] = []
+  let cursor: string | undefined
+  do {
+    const result = await agent.getAuthorFeed({ actor: identifier, limit: 100, cursor })
+    for (const p of result.data.feed) {
+      if (AppBskyFeedPost.isRecord(p.post.record) && p.post.author.handle === identifier && !p.reply && (!p.post.embed || AppBskyEmbedImages.isMain(p.post.embed))) {
+        feed.push(p)
+      }
+      cursor = result.data.cursor
+    }
+  } while (cursor && feed.length < 20)
+
+  return Promise.all(feed.map(p => {
+    const post = p.post.record as AppBskyFeedPost.Record
+    const embed = AppBskyEmbedImages.isMain(p.post.embed) ? p.post.embed : undefined
+    const text = new MagicString(post.text)
+    for (const facet of post.facets || []) {
+      const startIndex = [...post.text].findIndex(
+        (_, i) =>
+          Buffer.byteLength(post.text.slice(0, i + 1))
+          > facet.index.byteStart,
+      )
+      const endIndex = [...post.text].findIndex(
+        (_, i) =>
+          Buffer.byteLength(post.text.slice(0, i + 1))
+          >= facet.index.byteEnd,
+      )
+      for (const feature of facet.features) {
+        if (feature.$type === 'app.bsky.richtext.facet#link') {
+          text.appendRight(startIndex, `<a href="${feature.uri}">`)
+          text.appendLeft(endIndex === -1 ? post.text.length : endIndex + 1, '</a>')
+          continue
         }
+      }
+    }
 
-        text.replaceAll(/[<>&]/g, r => ({
-          '<': '&lt;',
-          '>': '&gt;',
-          '&': '&amp;',
-        }[r]!))
+    text.replaceAll(/[<>&]/g, r => ({
+      '<': '&lt;',
+      '>': '&gt;',
+      '&': '&amp;',
+    }[r]!))
 
-        for (const match of post.text.matchAll(/\n\n/g)) {
-          text.overwrite(match.index!, match.index! + 2, '</p><p>')
-        }
+    for (const match of post.text.matchAll(/\n\n/g)) {
+      text.overwrite(match.index!, match.index! + 2, '</p><p>')
+    }
 
-        text.prepend('<p>')
-        text.append('</p>')
+    text.prepend('<p>')
+    text.append('</p>')
 
-        return {
-          network: 'bluesky' as const,
-          accountLink: `https://staging.bsky.app/profile/${p.post.author.handle}`,
-          avatar: p.post.author.avatar,
-          handle: p.post.author.displayName,
-          createdAt: post.createdAt,
-          permalink:
-              `https://staging.bsky.app/profile/${p.post.author.handle}/post`
+    return {
+      network: 'bluesky' as const,
+      accountLink: `https://bsky.app/profile/${p.post.author.handle}`,
+      avatar: p.post.author.avatar,
+      handle: p.post.author.displayName,
+      createdAt: post.createdAt,
+      permalink:
+              `https://bsky.app/profile/${p.post.author.handle}/post`
               + p.post.uri.match(/(\/[^/]+)$/)?.[1],
-          html: text.toString().replace(/\n/g, '<br>'),
-          media: embed?.images?.map(i => ({
-            url: i.fullsize,
-            alt: i.alt,
-          })),
-        }
-      }),
-  )
+      html: text.toString().replace(/\n/g, '<br>'),
+      media: embed?.images?.map(i => ({
+        url: i.fullsize,
+        alt: i.alt,
+      })),
+    }
+  }))
 })
