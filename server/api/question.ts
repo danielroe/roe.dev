fde6f683d6dc42142b1603ef6caa94f262d02e2d
Daniel Roe <daniel@roe.dev>
2024-11-20 14:45:16 -0500

    feat: push ama questions to sanity

--- server/api/question.ts
+++ server/api/question.ts
@@ -1,10 +1,14 @@
 export default defineEventHandler(async event => {
   if (event.method === 'OPTIONS') return null
+  const sanity = useSanity()
 
   assertMethod(event, 'POST')
   const { question } = await readBody(event)
   if (!question) throw createError({ statusCode: 422 })
+
+  event.waitUntil(sanity.client.create({ _type: 'ama', content: question }).catch(console.error))
   event.waitUntil(createTypefullyDraft(event, question).catch(console.error))
+
   await sendEmail(event, 'Anonymous question', question)
   return null
 })
