7d99b6797e685b37a10054863477d67d58594f37
Daniel Roe <daniel@roe.dev>
2024-12-02 13:44:40 +0000

    chore(cms): add `answered` prop

--- cms/schemaTypes/ama.ts
+++ cms/schemaTypes/ama.ts
@@ -4,11 +4,30 @@ export default defineType({
   name: 'ama',
   title: 'Ask me anything',
   type: 'document',
+  preview: {
+    select: {
+      title: 'content',
+      answered: 'answered',
+    },
+    prepare ({ title, answered }) {
+      return {
+        title: title,
+        subtitle: answered ? '✅ Answered' : '🚧 Not answered',
+      }
+    },
+  },
   fields: [
     defineField({
       name: 'content',
       title: 'Content',
       type: 'string',
+      readOnly: true,
+    }),
+    defineField({
+      name: 'answered',
+      title: 'Answered',
+      type: 'boolean',
+      initialValue: false,
     }),
   ],
 })
