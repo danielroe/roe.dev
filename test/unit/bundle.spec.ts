52dc577348b3ed839f36f0e0a6ec1f3b63996edf
Daniel Roe <daniel@roe.dev>
2025-01-26 21:44:12 +0100

    chore: bump bundle size for new article

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -79,7 +79,7 @@ describe('project sizes', () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
     expect
       .soft(roundToKilobytes(stats.server.totalBytes))
-      .toMatchInlineSnapshot(`"426k"`)
+      .toMatchInlineSnapshot(`"428k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
