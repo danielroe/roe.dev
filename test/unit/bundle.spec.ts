33c9f0fde8254e37f088b212c9edeaa4599645fc
Daniel Roe <daniel@roe.dev>
2025-03-08 23:50:58 +0000

    chore: update bundle size

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -79,12 +79,12 @@ describe('project sizes', () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
     expect
       .soft(roundToKilobytes(stats.server.totalBytes))
-      .toMatchInlineSnapshot(`"418k"`)
+      .toMatchInlineSnapshot(`"427k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect
       .soft(roundToKilobytes(modules.totalBytes))
-      .toMatchInlineSnapshot(`"19014k"`)
+      .toMatchInlineSnapshot(`"18797k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
