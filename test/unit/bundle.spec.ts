72c1fc2d65f2ee92bd38a8160dc21e7a8e7f88f0
Daniel Roe <daniel@roe.dev>
2025-05-24 21:42:18 +0100

    chore: update bundle test for web mentions

--- test/unit/bundle.spec.ts
+++ test/unit/bundle.spec.ts
@@ -36,7 +36,7 @@ describe('project sizes', () => {
     stats.client = await analyzeSizes('**/*.js', publicDir)
     expect
       .soft(roundToKilobytes(stats.client.totalBytes))
-      .toMatchInlineSnapshot(`"493k"`)
+      .toMatchInlineSnapshot(`"496k"`)
     expect.soft(stats.client.files.map(f => f.replace(/\..*\.js/, '.js').replace(/_scripts\/.*\.js/, '_scripts/script.js')).sort())
       .toMatchInlineSnapshot(`
         [
