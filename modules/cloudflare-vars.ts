f7458e7ee78ed7fbef7a70b13a40e0bef323a52b
Daniel Roe <daniel@roe.dev>
2025-06-10 21:41:10 +0100

    fix: pre-populate cloudflare runtime vars

---
+++ modules/cloudflare-vars.ts
@@ -0,0 +1,37 @@
+import { useRuntimeConfig, defineNuxtModule, useNuxt } from 'nuxt/kit'
+import { kebabCase } from 'scule'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'cloudflare-vars',
+  },
+  setup () {
+    const nuxt = useNuxt()
+
+    nuxt.hook('nitro:config', config => {
+      const runtimeConfig = useRuntimeConfig()
+      const vars: Record<string, string> = {}
+
+      function walkConfig (obj: Record<string, any>, prefix = '') {
+        for (const [key, value] of Object.entries(obj)) {
+          if (typeof value === 'object' && value !== null) {
+            walkConfig(value, `${prefix}${key}.`)
+          }
+          else if (typeof value !== 'object') {
+            const varKey = kebabCase(`${prefix}${key}`).toUpperCase().replace(/-/g, '_')
+            vars[varKey] = value
+          }
+        }
+      }
+
+      walkConfig(runtimeConfig)
+
+      config.cloudflare ||= {}
+      config.cloudflare.wrangler ||= {}
+      config.cloudflare.wrangler.vars = {
+        ...vars,
+        ...config.cloudflare.wrangler.vars,
+      }
+    })
+  },
+})
