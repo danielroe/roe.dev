ce74f2c9ac460a58e5375935da8d2af0de148344
Daniel Roe <daniel@roe.dev>
2026-02-20 17:11:09 +0100

    perf: move markdown transforms to build-time

--- server/routes/bio.md.get.ts
+++ server/routes/bio.md.get.ts
@@ -2,12 +2,10 @@ import { rawPages } from '#md-raw-pages.json'
 import { pageMeta } from '#md-page-meta.json'
 
 export default defineEventHandler(() => {
-  const body = rawPages['bio'] || ''
-
   const md = [
     mdFrontmatter('/bio', pageMeta['/bio']!),
     '',
-    mdInternalLinks(mdCleanHtml(body)),
+    rawPages['bio'] || '',
     '',
   ].join('\n')
 
